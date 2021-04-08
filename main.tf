ae18119061c9c664d52acc5b0aa0c0c09b71d9db
Doug Zitzelsberger <dzitzelsberger@entrata.com>
2021-04-08 17:11:27 -0600

    Updated commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0577b787189839998"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data = <<-EOF
+              yum update -y
+              yum install httpd -y
+              echo "Hello, World!" >> /var/www/html/index.html"
+              systemctl enable httpd
+              systemctl start httpd
+              EOF
+
+  tags = {
+    Name = "terraform-instance-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
