d3ac6901a572387ac0d4319a20f887bae0d2694f
Doug Zitzelsberger <dzitzelsberger@entrata.com>
2021-04-09 13:51:41 -0600

    Updated commit

--- main.tf
+++ main.tf
@@ -3,11 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0577b787189839998"
-  instance_type = "t2.micro"
+  ami                    = "ami-0577b787189839998"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
-  user_data = <<-EOF
-              yum update -y
+  user_data              = <<-EOF
               yum install httpd -y
               echo "Hello, World!" >> /var/www/html/index.html"
               systemctl enable httpd
@@ -22,9 +21,9 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   ingress {
-    from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
